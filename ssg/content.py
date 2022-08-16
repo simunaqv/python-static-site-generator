55e5380df919a62062041224f0fcfc49e629c5d4
simunaqv <simunaqv@gmail.com>
2022-08-16 14:38:07 +0200

    Update content.py
    Fix

--- ssg/content.py
+++ ssg/content.py
@@ -7,7 +7,7 @@ from collections.abc import Mapping
 class Content(Mapping):
 
     __delimiter = r"^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @property
     def type(self):
